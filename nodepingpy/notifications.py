3e6e19eddd9730e4c43e554cb2ce32eb08781467
NodePing <support@nodeping.com>
2024-03-15 16:47:36 -0700

    Add docstrings

--- nodepingpy/notifications.py
+++ nodepingpy/notifications.py
@@ -15,6 +15,13 @@ ROUTE = "notifications"
 
 @dataclass
 class Notification:
+    """
+    Args:
+        id (str): Check id of the check for which you want to list notifications.
+        span (int): number of hours of notifications to retrieve.
+        limit (int): default 300, max 43201 - number of records to retrieve.
+        subaccounts (bool): if set, notifications sent to subaccounts will also be included.
+    """
     id: str | None = None
     span: int | None = None
     limit: int | None = 300
@@ -22,7 +29,19 @@ class Notification:
 
 
 def get(token: str, args: dict, customerid: str | None = None) -> dict:
-    """ """
+    """Get notifications on your NodePing account.
+
+    Get notifications according to a maximum limit (limit caps at 43201),
+    or over a certain amount of hours, and whether or not to include
+    subaccounts or not when fetching notifications.
+
+    https://nodeping.com/docs-api-notifications.html
+
+    Args:
+        token (str): NodePing API token
+        args (dict): Notifications class in this module.
+        customerid (str): subaccount ID
+    """
     data = asdict(args)
     data["token"] = token
 
