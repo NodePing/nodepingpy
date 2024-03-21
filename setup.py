2f32be5d5d4d0ae0e54a6423ee2bd9d731754c66
NodePing <support@nodeping.com>
2024-03-21 15:39:59 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+NAME = "nodepingpy"
+DESCRIPTION = "Python package for querying the NodePing API"
+URL = "https://github.com/NodePing/nodepingpy"
+EMAIL = "support@nodeping.com"
+AUTHOR = "NodePing"
+VERSION = "1.0.0"
+LICENSE = "MIT"
+
+setuptools.setup(
+    name=NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=URL,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
