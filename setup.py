aa66c1c31a5e3875fc2486c17d13cb145e8ef3e3
NodePing <support@nodeping.com>
2024-03-27 12:59:07 -0700

    Bump to 1.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ DESCRIPTION = "Python package for querying the NodePing API"
 URL = "https://github.com/NodePing/nodepingpy"
 EMAIL = "support@nodeping.com"
 AUTHOR = "NodePing"
-VERSION = "1.0.1"
+VERSION = "1.0.2"
 LICENSE = "MIT"
 
 setuptools.setup(
