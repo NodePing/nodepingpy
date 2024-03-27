ecf72e401fd80f593986d5971b8d116047b5cb25
NodePing <support@nodeping.com>
2024-03-27 12:58:46 -0700

    Update __init__.py

---
+++ nodepingpy/__init__.py
@@ -0,0 +1,21 @@
+from . import (
+    accounts,
+    checks,
+    contactgroups,
+    contacts,
+    diagnostics,
+    information,
+    maintenance,
+    notificationprofiles,
+    notifications,
+    results,
+    schedules,
+)
+from .nptypes import (
+    checktypes,
+    contacttypes,
+    diagtypes,
+    maintenancetypes,
+    notificationtypes,
+    resulttypes,
+)
